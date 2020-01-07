e8bd01e507a6f85c4a19ddbb47cd237f26022f42
Rodrigo Antunes <rodrigoka@live.com>
2020-01-07 11:50:21 +0000

    firebase remove conection

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyChOf2v2XDiVEVMC5hqU3ygBQDxcautQHg",
-  authDomain: "careoncare-4283c.firebaseapp.com",
-  databaseURL: "https://careoncare-4283c.firebaseio.com",
-  projectId: "careoncare-4283c",
-  storageBucket: "careoncare-4283c.appspot.com",
-  messagingSenderId: "471874617432",
-  appId: "1:471874617432:web:1095cc9a7fef042a8c8a2d"
+  // conexao firebase
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 });
 
 firebase.firestore().enablePersistence();
